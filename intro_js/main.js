e4402b5e63eaa6c065b4e4f65a120a8d30625c1a
kithekadk <dankinyi99@gmail.com>
2024-05-13 12:59:35 +0300

    added introduction to JS

---
+++ intro_js/main.js
@@ -0,0 +1,48 @@
+var username = "cohort 26";
+
+
+
+const lastname = "Kadima"
+
+console.log(lastname);
+
+
+// username = 'Clinton Omari'
+
+// console.log(username)
+    
+/*let firstname
+
+
+function findVariable(){
+    const firstname = "Emmanuel"
+
+    console.log(firstname);
+
+    
+}
+
+findVariable()*/
+
+let users = ['Alvin', 'Carolyne', 'Charity', 'Clinton']
+
+let age = 20
+age = 24
+
+let trainess = [
+    {
+        name: 'Felix',
+        age: 20
+    },
+    {
+        name: 'Eric',
+        age: 20
+    },
+    {
+        name: 'Eric',
+        age: 20
+    },
+
+]
+
+console.log(typeof(users));
